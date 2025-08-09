501dbe6e5fdbc3bbae4ab3261f0c865ff8153b09
chathuniimasha <chathuniimasha200267@gmail.com>
2025-08-09 10:59:24 +0530

    Day 11🚀

---
+++ src/pages/testPage.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function TestPage(){
+    //let const=0;
+    const[count,setCount] = useState(0);
+    
+    function increment(){
+        setCount(count+1);
+    }
+    function decrement(){
+        setCount(count-1);
+    }
+    //setCount(5)  notuse that because again and again refresh not run
+
+    return(
+        <div className='w-full h-screen bg-amber-200 flex justify-center items-center'>
+            <div className='w-[400px] h-[400px] bg-white flex flex-col justify-center items-center'>
+                <h1 className='text-5xl front-bold'>{count}</h1> 
+                <div className='w-full flex justify-center items-center border h-[100px]'> 
+                    <button onClick={decrement} className='w-[100px] bg-blue-500 h-[45px] text-3xl mx-2 flex justify-center items-center text-white rounded-full'>-</button>
+                    <button onClick={increment} className='w-[100px] bg-blue-500 h-[45px] text-3xl mx-2 flex justify-center items-center text-white rounded-full'>+</button>
+
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+ 
