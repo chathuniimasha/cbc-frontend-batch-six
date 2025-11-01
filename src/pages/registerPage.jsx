9d9e9d55e7b804fc5129297863de6fe979643350
chathuniimasha <chathuniimasha200267@gmail.com>
2025-11-01 14:30:43 +0530

    register done👍

--- src/pages/registerPage.jsx
+++ src/pages/registerPage.jsx
@@ -1,7 +1,106 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+ 
 export default function RegisterPage(){
+    const [firstName,setFirstName] = useState("")
+    const [lastName,setLastName] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const navigate = useNavigate()
+  
+
+    function register(){
+
+        if (!firstName || !lastName || !email || !password) {
+        toast.error("All fields are required");
+        return;
+    }
+        
+        axios.post(import.meta.env.VITE_BACKEND_URL +"/api/users/",{
+            firstName:firstName,
+            lastName:lastName,
+            email:email,
+            password:password
+        }).then(
+            (response)=>{
+               
+                toast.success("Register Successful!");
+                navigate("/login");
+
+            
+            }
+        ).catch(
+            (error)=>{
+                console.log(error)
+                
+                toast.error("Register Failed")
+            }
+        )
+    }
+    
+    
     return(
-        <div>
-            <span>Register Page</span>
+        <div className="w-full h-screen bg-[url(/loginbg.jpg)] bg-cover bg-center flex justify-center items-center">
+            <div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px] relative gap-[20px] text-white flex flex-col items-center justify-center">
+                <h1 className="absolute top-[5px] text-2xl font-bold text-center my-5">Register</h1>
+
+                <div className="w-[350px] flex flex-col">
+                        <span className="text-lg">First Name</span>
+                        <input onChange={
+                            (e)=>{
+                                setFirstName(e.target.value)
+
+                        }
+                    } type="text" className="w-[350px] h-[40px] border border-white
+                         rounded-xl"/>
+                    </div>
+                    <div className="w-[350px] flex flex-col">
+                        <span className="text-lg">Last Name</span>
+                        <input onChange={
+                            (e)=>{
+                                setLastName(e.target.value)
+
+                        }
+                    } type="text" className="w-[350px] h-[40px] border border-white
+                         rounded-xl"/>
+                    </div>
+
+                    <div className="w-[350px] flex flex-col">
+                        <span className="text-lg">Email</span>
+                        <input onChange={
+                            (e)=>{
+                                setEmail(e.target.value)
+
+                        }
+                    } type="text" className="w-[350px] h-[40px] border border-white
+                         rounded-xl"/>
+                    </div>
+                    <div className="w-[350px] flex flex-col">
+                        <span className="text-lg">Password</span>
+                        <input onChange={
+                            (e)=>{
+                                setPassword(e.target.value)
+                               
+                            }
+                        } type="password" className="w-[350px] h-[40px] border border-white
+                         rounded-xl"/>
+                    </div>
+
+                    <button  onClick={register} className="w-[350px] h-[40px] bg-blue-500 rounded-xl text-white text-lg mt-5 hover:bg-blue-600 transition all duration-300">
+                        Register
+                    </button>
+                    
+
+                    
+                
+                
+
+            </div>
+            
+
         </div>
     )
 }
